79eeaae0e6b35edef0e09d538461dfed76d65c37
PeerSyedHussain <syedhussain7558@gmail.com>
2020-11-24 21:17:00 +0530

    commiting header

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Navbar,Nav,NavDropdown,Form,FormControl,Button } from 'react-bootstrap';
-import logo from './../assets/images/mr-med-logo.png'
+import { Navbar,Form,FormControl,Button } from 'react-bootstrap';
+import logo from './../assets/images/mr-med-logo.png';
 
 class Header extends Component {
+  
     render() {
         return (
             <section id='header'>
@@ -16,13 +17,28 @@ class Header extends Component {
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                        <Form inline>
-                            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                            <Button variant="outline-success">Search</Button>
-                        </Form>
-                        <Button className='upload-btn'>
-                            Upload Prescription
-                        </Button>
+                        <div className='col-md-8'>
+                            <Form className='col-md-4'>
+                                <select className='city-select'>
+                                    <option value=''>select your city</option>
+                                    <option value='choco'>Choco</option>
+                                    <option value='choco1'>Choco1</option>
+                                    <option value='choco2'>Choco2</option>
+                                    <option value='choco3'>Choco3</option>
+                                    <option value='choco4'>Choco4</option>
+                                </select>
+                            </Form>
+                            
+                            <Form inline className='col-md-8'>
+                                <FormControl type="text" placeholder="Search for medicines and health products" className="mr-sm-2 medicine-search-box" />
+                            </Form>
+                        </div>
+                        <div className='col-md-4'>
+                            <Button className='upload-btn'>
+                                Upload Prescription
+                            </Button>
+                        </div>
+                       
                     </Navbar.Collapse>
                 </Navbar>
             </section>
